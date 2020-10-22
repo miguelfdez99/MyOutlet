88f1cc59b42633c2327e1289ab4341828579f2d0
Miguel Fernandez <miguelfdez99@gmail.com>
2020-10-23 01:06:58 +0200

    Avance de código, closes #8

--- src/item.js
+++ src/item.js
@@ -1,52 +1,140 @@
-class Item{
-  constructor(name,type,size,brand,color,price,discount,season){
-      this.name = name;
-      this.type = type;
-      this.size = size;
-      this.brand = brand;
-      this.color = color;
-      this.price = price;
-      this.discount = discount;
-      this.season = season;
+class Item {
+
+  /**
+   * Constructor
+   * @param {String} type - Type of the item
+   * @param {String} size - Size of the item
+   * @param {String} brand - Brand of the item
+   * @param {String} color - Color of the item
+   * @param {Number} price - Price of the item
+   * @param {String} season - Season of the item
+   */
+
+  constructor(nType, nSize, nBrand, nColor, nPrice, nSeason) {
+    this.type = nType;
+    this.size = nSize;
+    this.brand = nBrand;
+    this.color = nColor;
+    this.price = nPrice;
+    this.season = nSeason;
+    this.review = new Array();
   }
+
+  //Get
+
+  getType() {
+    return this.type;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getBrand() {
+    return this.brand;
+  }
+
+  getColor() {
+    return this.color;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  getSeason() {
+    return this.season;
+  }
+
+  getReview(){
+    return this.review;
+  }
+
+  //Set
+
+  setType(t) {
+    if (t=="HOODIE" || t=="T-SHIRT" || t=="JEANS" || t=="JOGGERS" || t=="JACKET" ||
+      t=="SHOES" || t=="TROUSERS" || t=="SHIRTS"){
+        this.type = t;
+    }else{
+      throw new Error("Type name not valid");
+    }
+  }
+
+  setSize(s) {
+    if (s == "XS" || s == "S" || s == "M" ||
+      s == "L" || s == "XL" || s == "XXL" || s == "XXXL") {
+      this.size = s;
+    } else {
+      throw new Error("Size format not valid");
+    }
+  }
+
+  setBrand(b) {
+    if (b == "ADIDAS" || b == "ASICS" || b == "FILA" || b == "JOMA" || b == "KAPPA" || b == "NIKE" ||
+      b == "PUMA" || b == "NEW_BALANCE") {
+      this.brand = b;
+    } else {
+      throw new Error("Brand name not valid");
+    }
+  }
+
+  setColor(c) {
+    if (c == "WHITE" || c == "BLACK" || c == "BLUE" || c == "RED" || c == "GREEN" ||
+      c == "YELLOW" || c == "PURPLE" || c == "BROWN" || c == "MULTICOLOR") {
+      this.color = c;
+    } else {
+      throw new Error("Color not valid");
+    }
+  }
+
+  setSeason(se) {
+    if (se == "FALL_WINTER" || se == "SPRING_SUMMER") {
+      this.season = se;
+    } else {
+      throw new Error("Season name not valid");
+    }
+  }
+
+  //Method to add a new review
+
+  addReview(re) {
+    var exists = false;
+    this.review.forEach(element => {
+      if (element == re) {
+        exists = true;
+      }
+    });
+    if (exists == true) {
+      throw new Error("This review already exists")
+    } else {
+      this.review.push(re);
+    }
+  }
+
+  //Method to update some fields of the item
+
+  updateItem(nSize, nColor, nPrice) {
+      this.size = nSize;
+      this.color = nColor;
+      this.price = nPrice;
 }
+
+  //Method to check the information about an item
+
+  checkItem() {
+    var description = ("Information about the item" +
+      "\n Type: " + this.type +
+      "\n Size: " + this.size +
+      "\n Brand: " + this.brand +
+      "\n Color: " + this.color +
+      "\n Price: " + this.price +
+      "\n Season: " + this.season);
+
+      return description;
+    }
+
+}
+
+
 module.exports = Item;
-
-export const type{
-  HOODIES = "hoodies",
-  JACKET = "jacket",
-  JOGGERS = "joggers",
-  SHIRTS = "shirts",
-  SHORTS = "shorts",
-  SHOES = "shoes",
-  TROUSERS = "trousers",
-  T_SHIRTS = "t_shirts",
-  UNDERWEAR = "underwear"
-}
-
-export const size{
-  XS = "xs",
-  SMALL = "s",
-  MEDIUM = "m",
-  LARGE = "l",
-  XL = "xl",
-  XXL = "xxl",
-  3XL = "3xl"
-
-}
-
-export const brand{
-  ADIDAS = "adidas",
-  ASICS = "asics",
-  FILA = "fila",
-  JOMA = "joma",
-  KAPPA = "kappa",
-  NIKE = "nike",
-  PUMA = "puma",
-  NEW_BALANCE = "new_balance"
-}
-
-export const season{
-  SPRING_SUMMER = "spring_winter",
-  FALL_WINTER = "fall_winter"
-}
--- src/myoutlet.js
+++ src/myoutlet.js
@@ -1,15 +1,42 @@
-require("./item.js")
+var Item = require("./item.js");
+
 
 class MyOutlet{
-
-  constructor(){
-    Item item_name;
+  constructor() {
+    this.items = new Array();
   }
 
-addItem(name,type,size,brand,color,price,discount,season){}
-deleteItem(name,type,size,brand,color,price,discount,season){}
-updateItem(name,type,size,brand,color,price,discount,season){}
-checkItem(){}
+  //Method to add a new item to the store
 
+  addItem(item) {
+    var exists = false;
+    this.items.forEach(element => {
+      if (item.type == element.type && item.size == element.size && item.brand == element.brand &&
+      item.color == element.color && item.price == element.price && item.season == element.season){
+        exists = true;
+        throw new Error("This item already exists");
+      }
+    });
+    if (exists == false) {
+      this.items.push(item);
+    }
+  }
+
+  getItem() {
+    return this.items;
+  }
+
+
+  //Method to delate an item of the store
+
+  deleteItem(item) {
+    if(this.items.length > 0){
+      this.items.pop(item);
+    }else{
+      throw new Error("There is not items in the store");
+    }
+  }
 
 }
+
+module.exports = MyOutlet;
