bd3bf064d32003fbef202385e1e45a78270df0c8
Miguel Fernandez <miguelfdez99@gmail.com>
2020-12-08 18:13:13 +0100

    mejoro la función #43

--- functions/getBrand.js
+++ functions/getBrand.js
@@ -4,11 +4,12 @@ function gBrand(b){
     item = "";
     for(i = 1; i <= 6; i++){
         if (data['items'][i]["brand"] == b){
-          item += "\n Type:" + data['items'][i]["type"][i] +
+          item += data['items'][i]["brand"] + ":" +
+          "\n Type:" + data['items'][i]["type"] +
           "\n Size: " + data['items'][i]["size"] +
           "\n Color: " + data['items'][i]["color"] +
           "\n Price: " + data['items'][i]["price"] +
-          "\n Season: " + data['items'][i]["season"] + "";
+          "\n Season: " + data['items'][i]["season"] + "\n";
     }
   }
   return item;
