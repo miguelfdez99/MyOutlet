cabfd696c01254040745f54c0901311f2127beb5
Miguel Fernandez <miguelfdez99@gmail.com>
2020-12-08 13:29:22 +0100

    actualizo función para conseguir los items de una marca #43

--- functions/brand.js
+++ functions/brand.js
@@ -1,41 +1,40 @@
 const brand = require('./wBrand')
 
 exports.handler = async function(event, context){
-
+  try{
     var body = JSON.parse(event.body);
-    if(body.message != undefined){
-    var msg = body.message.text;
-    var id_chat = body.message.chat.id;
+    var {chat, text} = body.message;
     var item = "";
 
-    if(msg == "/start"){
-      item = "¡Bienvenido, utilice los comando como ayuda!"
-    }
-    else if(msg == "/puma"){
-      item = brand.wBrand("PUMA");
-    }else if(msg == "/adidas"){
-      item = brand.wBrand("ADIDAS");
-    }else if(msg == "/kappa"){
-      item = brand.wBrand("KAPPA");
-    }else if(msg == "/nike") {
-      item = brand.wBrand("NIKE");
-    }else if(msg = "/joma"){
-      item = brand.wBrand("JOMA")
-    }else{
-      item = "Para consultar los artículos de cada marca tiene los siguientes comandos: /puma para las prendas de marca Puma, /adidas para las prendas de marca Adidas, /kappa ra las prendas de marca Kappa, /nike para las prendas de marca Nike y /joma para las prendas de marca Joma";
-    }
+    if(text){
+      switch(text){
+        case "/puma":
+          item = brand.wBrand("PUMA");
+          break;
+        case "/adidas":
+          item = brand.wBrand("ADIDAS");
+          break;
+        case "/kappa":
+          item = brand.wBrand("KAPPA");
+          break;
+        case "/nike":
+          item = brand.wBrand("NIKE");
+          break;
+        case "/joma":
+          item = brand.wBrand("JOMA");
+          break;
+        default:
+          item = "Para ver los objetos de marca Puma utilice /puma, para Adidas /adidas , para Kappa /kappa. para Nike /nike y para Joma /joma"
+        break;
+      }
     return {
       statusCode: 200,
       body: JSON.stringify({text:item, method:'sendMessage', chat_id:chat.id}),
       headers:{
         'Content-Type': 'application/json; charset=utf-8'
-      }
-    };
-  }else{
-    item = "";
-    return{
-      statusCode: 200.
-      body: item.toString()
+        }
+      };
     }
-  }
+  }catch(error){
+    }
 }
