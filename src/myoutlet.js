e67e6a9dfb5e384cdfa8daef73239606422de9b7
Miguel Fernandez <miguelfdez99@gmail.com>
2020-12-27 02:21:44 +0100

    método para mostrar el tipo de prenda de una temporada #38

--- src/myoutlet.js
+++ src/myoutlet.js
@@ -1,7 +1,7 @@
 var Item = require("./item.js");
 var Accessory = require("./accessories.js");
 
-class MyOutlet{
+class MyOutlet {
   constructor() {
     this.items = new Array();
     this.accessories = new Array();
@@ -12,7 +12,7 @@ class MyOutlet{
     var exists = false;
     this.items.forEach(element => {
       if (item.type == element.type && item.size == element.size && item.brand == element.brand &&
-      item.color == element.color && item.price == element.price && item.season == element.season){
+        item.color == element.color && item.price == element.price && item.season == element.season) {
         exists = true;
         throw new Error("This item already exists");
       }
@@ -29,18 +29,18 @@ class MyOutlet{
 
   //Method to delate an item of the store
   deleteItem(item) {
-    if(this.items.length > 0){
+    if (this.items.length > 0) {
       this.items.pop(item);
-    }else{
+    } else {
       throw new Error("There is not items in the store");
     }
   }
 
   //Method to update one item of the store
-  updatedItem(type,size,brand,color,price,season){
+  updatedItem(type, size, brand, color, price, season) {
     var exists = false;
-    for(var i in this.items){
-      if (this.items[i].getType()){
+    for (var i in this.items) {
+      if (this.items[i].getType()) {
         exists = true;
       }
     }
@@ -52,49 +52,48 @@ class MyOutlet{
       this.items[i].price = price;
       this.items[i].season = season;
       return this.items[i];
-    }
-    else{
+    } else {
       throw new Error("Error updating the item");
     }
   }
 
-//Method to show every item
-showItems(){
-  if(this.items.length > 0){
-    var _items = new Array();
-    for(var i in this.items){
-      _items.push(this.items[i].checkItem());
+  //Method to show every item
+  showItems() {
+    if (this.items.length > 0) {
+      var _items = new Array();
+      for (var i in this.items) {
+        _items.push(this.items[i].checkItem());
+      }
+      return _items;
+    } else {
+      throw new Error("There are no items")
     }
-    return _items;
-  }else{
-    throw new Error("There are no items")
-  }
 
   }
 
   //Method to show every type existing in the store
-  showType(){
+  showType() {
     var _type = new Array();
-    for(var i in this.items){
+    for (var i in this.items) {
       _type.push(this.items[i].getType());
     }
     return _type;
   }
 
   //Method to show every size existing in the store
-  showBrand(){
+  showBrand() {
     var _brand = new Array();
-    for(var i in this.items){
+    for (var i in this.items) {
       _brand.push(this.items[i].getBrand());
     }
     return _brand;
-    }
+  }
 
   //Method to add a new accessory
-  addAccessory(accessory){
+  addAccessory(accessory) {
     var exists = false;
     this.accessories.forEach(element => {
-      if (accessory.type == element.type && accessory.description == element.description){
+      if (accessory.type == element.type && accessory.description == element.description) {
         exists = true;
         throw new Error("This accessory already exists");
       }
@@ -106,28 +105,45 @@ showItems(){
 
   //Method to delate an item of the store
   deleteAccesory(accesory) {
-    if(this.accessories.length > 0){
+    if (this.accessories.length > 0) {
       this.accessories.pop(accesory);
-    }else{
+    } else {
       throw new Error("There are no items in the store");
-     }
+    }
   }
 
 
   //Method to show information about certain brand
   brandInfo(b) {
-      let brand = ""
-      for(var i in this.items){
-        if (this.items[i].getBrand() ==  b){
-          brand += "Type:" + this.items[i].getType() + ", " +
+    let brand = ""
+    for (var i in this.items) {
+      if (this.items[i].getBrand() == b) {
+        brand += "Type:" + this.items[i].getType() + ", " +
           "Size: " + this.items[i].getSize() + ", " +
           "Color: " + this.items[i].getColor() + ", " +
           "Price: " + this.items[i].getPrice() + ", " +
           "Season: " + this.items[i].getSeason() + " ";
+      }
+    }
+    return brand;
+  }
+
+
+  //
+  seasonType(s) {
+    if (s == "FALL_WINTER" || s == "SPRING_SUMMER") {
+      let season = new Array();
+      for (var i in this.items) {
+        if (this.items[i].getSeason() == s) {
+          season.push(this.items[i].getType());
+        }
+      }
+      return season;
+    } else {
+      throw new Error("Season name it's not right")
     }
   }
-  return brand;
-  }
+
 
 }
 
